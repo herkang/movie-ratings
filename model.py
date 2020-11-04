9c3dd22f416250155511dec77cfcf5c711468c35
Kang Her <her.kang@yahoo.com>
2020-11-04 20:49:31 +0000

    Files

--- model.py
+++ model.py
@@ -42,7 +42,7 @@ class Rating(db.Model):
 
     rating_id = db.Column(db.Integer, autoincrement = True, primary_key = True)
     score = db.Column(db.Integer)
-    movie_id = db.Column(db.Integer, db.ForeignKey('moves.movie_id'))
+    movie_id = db.Column(db.Integer, db.ForeignKey('movies.movie_id'))
     user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
 
     movie = db.relationship('Movie', backref = 'ratings')
