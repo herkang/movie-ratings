fe555bab88f906bdc87d7deaf72be012ec154ac9
Kang Her <her.kang@yahoo.com>
2020-11-04 20:28:26 +0000

    Created User class foor users table

---
+++ model.py
@@ -0,0 +1,39 @@
+"""Models for movie ratings app."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    """A user."""
+
+    #create user table named users with 3 cloumns (user_id, email, password)
+
+    __tablename__ = 'users'
+    user_id = db.Column(db.Integer, autoincrement= True, primary_key = True)
+    email = db.Column(db.String, unique = True)
+    password = db.Colum(db.String)
+
+    #returns user id and email 
+    def __repr__(self):
+        return f'<User user_id = {self.user_id} email = {self.email}>'
+
+def connect_to_db(flask_app, db_uri='postgresql:///ratings', echo=True):
+    flask_app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+    flask_app.config['SQLALCHEMY_ECHO'] = echo
+    flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.app = flask_app
+    db.init_app(flask_app)
+
+    print('Connected to the db!')
+
+
+if __name__ == '__main__':
+    from server import app
+
+    # Call connect_to_db(app, echo=False) if your program output gets
+    # too annoying; this will tell SQLAlchemy not to print out every
+    # query it executes.
+
+    connect_to_db(app)
